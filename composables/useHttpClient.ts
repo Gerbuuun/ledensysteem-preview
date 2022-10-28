9c1e8e7c51490c047653516ae935a12a65acad66
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-10-28 20:50:55 +0200

    Fix api url environment variable 7

--- composables/useHttpClient.ts
+++ composables/useHttpClient.ts
@@ -1,12 +1,12 @@
 import useAlertStore from "~/stores/useAlertStore";
 import {FetchOptions} from "ohmyfetch";
-
+import {useRuntimeConfig} from "#app";
 
 export const useHttpClient = () => {
   return {
     async apiRequest<T>(url: string, config?: FetchOptions): Promise<T> {
       try {
-        const data = await $fetch<T>(url, config);
+        const data = await $fetch<T>(useRuntimeConfig().public.PE_API_URL + url, config);
         return data as T;
       } catch (error) {
         if (error.response) {
